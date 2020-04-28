4834885c155a40dd07a70857af60b590506548c6
tungondugi <tungondugi@gmail.com>
2020-04-28 22:03:47 +0530

    Python equivalent of Linux/Unix 'cat' command

---
+++ cat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""Written by nognut"""
+"""The following python script is the python equivalent for Linux/Unix command 'cat'."""
+import sys
+def cat(filename):
+    with open(filename, 'r') as file:
+        for e in file.readlines():
+            print(e)
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    cat(filename)
